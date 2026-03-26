9bd3a80aca47bcdbeda667f2d9dd2dd3c8a9ba22
hdegroote <75906619+HDegroote@users.noreply.github.com>
2026-03-26 17:06:39 +0100

    Adapt to new contract

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ class HyperMultisig {
 
       const signResponses = []
       for (const response of responses) {
-        const res = cenc.decode(CoreSign.Response, z32.decode(response))
+        const res = SignRequest.decodeResponse(z32.decode(response))
         await CoreSign.verify(z32.decode(response), z32.decode(request), res.publicKey)
         const publicKeyHex = res.publicKey.toString('hex')
         signResponses[publicKeyHex] = res
@@ -240,7 +240,7 @@ class HyperMultisig {
 
       const signResponses = []
       for (const response of responses) {
-        const res = cenc.decode(CoreSign.Response, z32.decode(response))
+        const res = SignRequest.decodeResponse(z32.decode(response))
         await CoreSign.verify(z32.decode(response), z32.decode(request), res.publicKey)
         const publicKeyHex = res.publicKey.toString('hex')
         signResponses[publicKeyHex] = res
