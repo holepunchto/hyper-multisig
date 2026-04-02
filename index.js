b1a1c2f4f9ff936ffe5f9303009d14156f53c52c
hdegroote <75906619+HDegroote@users.noreply.github.com>
2026-04-02 09:25:11 +0200

    Expose version

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ class HyperMultisig {
     namespace,
     srcCore,
     length,
-    { force = false, quorum, peerUpdateTimeout } = {}
+    { force = false, quorum, peerUpdateTimeout, version } = {}
   ) {
     return new HyperMultisigRunner(async (runner) => {
       await srcCore.ready()
@@ -88,7 +88,7 @@ class HyperMultisig {
       if (!force) await verifyCoreRequestable(srcCore, length, { peerUpdateTimeout })
 
       const manifest = getManifest(publicKeys, namespace, { quorum })
-      const request = await SignRequest.generate(srcCore, { manifest, length })
+      const request = await SignRequest.generate(srcCore, { manifest, length, version })
       return { manifest, request }
     })
   }
@@ -98,7 +98,7 @@ class HyperMultisig {
     namespace,
     srcDrive,
     length,
-    { force = false, quorum, peerUpdateTimeout } = {}
+    { force = false, quorum, peerUpdateTimeout, version } = {}
   ) {
     return new HyperMultisigRunner(async (runner) => {
       await srcDrive.ready()
@@ -126,7 +126,7 @@ class HyperMultisig {
 
       runner.emit('creating-drive')
       const manifest = getManifest(publicKeys, namespace, { quorum })
-      const request = await SignRequest.generateDrive(srcDrive, { manifest, length })
+      const request = await SignRequest.generateDrive(srcDrive, { manifest, length, version })
       return { manifest, request }
     })
   }
