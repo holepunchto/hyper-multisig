720a567ef3654e029f271b1ec877e3086452d2fd
Marco <maidh91@gmail.com>
2026-05-14 20:22:24 +1000

    Commit errors (#28)
    * test-reject-bad-batch
    
    * update test
    
    * COMMIT_FAILED
    
    * compare w batch length
    
    * COMMIT_FAILED
    
    * Stop replication
    
    * treeHashFromStorage
    
    * treeHashFromStorage
    
    * remove solo
    
    * lint
    
    * if (result.length !== batch.length) throw MultisigError.COMMITTED_LENGTH_MISMATCH()
    remove

--- test.js
+++ test.js
@@ -326,6 +326,95 @@ test('sign core multiple times w/ partial replication from previous sign', async
   }
 })
 
+test('sign core rejects different batch recommit w/ partial replication from previous sign', async (t) => {
+  t.timeout(120000)
+
+  const { store, signers, multisig, publicKeys, namespace } = await setupTest(t)
+  const { manifest, core } = await multisig.createCore(publicKeys, namespace)
+
+  const fromCore = store.get({ name: 'fromCore' })
+  t.teardown(() => fromCore.close())
+  await fromCore.append(b4a.from('0'))
+  await fromCore.append(b4a.from('1'))
+  await fromCore.append(b4a.from('2'))
+  await fromCore.append(b4a.from('3'))
+  await fromCore.append(b4a.from('4'))
+  await fromCore.append(b4a.from('5'))
+
+  const { signatures } = await requestAndSign(signers, fromCore, manifest)
+
+  await signCore(core, fromCore, signatures, { commit: true })
+
+  // Create new storage to simulate separate peer
+  const store2 = new Corestore(await t.tmp())
+  t.teardown(() => store2.close(), { order: 4000 })
+
+  const localCore = store2.get({ manifest })
+  t.teardown(() => localCore.close())
+  t.is(localCore.length, 0, '2nd signer core is new')
+
+  const s1 = store.replicate(true)
+  const s2 = store2.replicate(false)
+  t.teardown(() => {
+    s1.destroy()
+    s2.destroy()
+  })
+  s1.pipe(s2).pipe(s1)
+
+  await once(localCore, 'append')
+
+  // Sparsely populate localCore with blocks: 0, 1, 4
+  await localCore.get(0)
+  await localCore.get(1)
+  await localCore.get(4)
+
+  t.is(localCore.length, core.length, 'same lengths')
+  t.alike(localCore.key, core.key, 'same key')
+
+  // Stop replication
+  s1.destroy()
+  s2.destroy()
+
+  t.alike(await Hypercore.treeHashFromStorage(localCore), await core.treeHash(), 'same treeHash')
+  t.absent(await localCore.has(0, core.length), '2nd signer core is missing blocks')
+
+  const differentFromCore = store.get({ name: 'differentFromCore' })
+  t.teardown(() => differentFromCore.close())
+  await differentFromCore.append(b4a.from('0'))
+  await differentFromCore.append(b4a.from('1'))
+  await differentFromCore.append(b4a.from('2'))
+  await differentFromCore.append(b4a.from('different-3'))
+  await differentFromCore.append(b4a.from('4'))
+  await differentFromCore.append(b4a.from('different-5'))
+
+  t.is(differentFromCore.length, fromCore.length, 'different batch length matches')
+  t.unlike(await differentFromCore.treeHash(), await fromCore.treeHash(), 'second batch differs')
+
+  await t.exception(
+    () => signCore(localCore, differentFromCore, signatures, { commit: true }),
+    /COMMIT_FAILED/,
+    'invalid commit throws commit failed'
+  )
+
+  t.is(localCore.length, core.length, 'length not updated after invalid commit')
+  t.alike(localCore.key, core.key, 'same key after invalid commit')
+  t.alike(
+    await Hypercore.treeHashFromStorage(localCore),
+    await core.treeHash(),
+    '2nd signer core treeHash is not updated after invalid commit'
+  )
+  t.absent(
+    await localCore.has(0, core.length),
+    '2nd signer core is still missing blocks after invalid commit'
+  )
+
+  const batch = await signCore(localCore, fromCore, signatures, { commit: true })
+  t.is(batch.key, idEnc.normalize(core.key), 'batch key is correct')
+  t.is(batch.length, fromCore.length, 'batch length is correct')
+  t.is(batch.treeHash, idEnc.normalize(await fromCore.treeHash()), 'batch treeHash is correct')
+  t.ok(await localCore.has(0, fromCore.length), '2nd signer core has all blocks')
+})
+
 test('sign core remotely', async (t) => {
   t.timeout(120000)
   const { store, swarm, store2, swarm2, multisig, multisig2, signers, publicKeys, namespace } =
